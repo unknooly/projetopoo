93a6987fbfd6db428546f6c7c6b5c464a815856b
unknooly <97864953+unknooly@users.noreply.github.com>
2025-06-02 21:19:42 -0300

    configurações

--- program.ts
+++ program.ts
@@ -2,6 +2,6 @@ import StartUp from "./startUp";
 let port="5000" // ideal seria usar const?
 
 StartUp.app.listen(port,function(){
-    console.log(`Servidor rodando na porta: ${port}\nLink: http://localhost:${port}`)
+    console.log(`Servidor rodando na porta: ${port}\n\nhttp://localhost:${port}`)
 })
 
